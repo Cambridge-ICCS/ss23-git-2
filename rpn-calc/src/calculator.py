0aac0f53fb8a22330741a48a9947939c1c6a7e3b
Ben Orchard <thefirstmuffinman@gmail.com>
2023-07-06 09:13:40 +0100

    initial commit: add basic RPN calculator
    Parses string input and handles a handful of operators.
    Includes a couple of unit tests.

---
+++ rpn-calc/src/calculator.py
@@ -0,0 +1,49 @@
+from stack import Stack
+
+
+class Calculator:
+    '''
+    A simple RPN calculator
+    '''
+
+    def __init__(self):
+        self.the_stack = Stack()
+
+    def parse(self, token: str):
+        '''
+        Process the token.
+        Operands pop the required number of tokens from the self.the_stack and
+        push the operand result.
+        "p" or "" (empty string) pop and print the top-most self.the_stack
+        element.
+        Anything else is assumed to be a float, and is pushed.
+        '''
+        match token:
+            case "+":
+                self.the_stack.push(
+                    self.the_stack.pop() + self.the_stack.pop()
+                )
+            # We could add the "-" and "/" operands during the presentation, as
+            # copy/pasting the "+" operand naturally leads to the wrong order
+            # (as it is here).
+            #
+            # Or we could note absence of tests for "-" and "/" and implement
+            # them and then find results are wrong.
+            case "-":
+                self.the_stack.push(
+                    self.the_stack.pop() - self.the_stack.pop()
+                )
+            case "*":
+                self.the_stack.push(
+                    self.the_stack.pop() * self.the_stack.pop()
+                )
+            case "/":
+                self.the_stack.push(
+                    self.the_stack.pop() / self.the_stack.pop()
+                )
+            case "p":
+                token = self.the_stack.pop()
+                print(token)
+                return token
+            case _:
+                self.the_stack.push(float(token))
